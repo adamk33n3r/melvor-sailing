8b655d1a0a302328d63144fec0fb353def4a456c
Adam Keenan <adam.g.keenan@gmail.com>
2024-10-23 00:45:53 -0400

    new version of webpack remove code block that supports mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/ts/setup.ts',
   watchOptions: {
     poll: true
